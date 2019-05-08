da8623df0ece13506c21250aa3be2037b1082d84
Phospheness <46403831+Phospheness@users.noreply.github.com>
2019-05-08 11:38:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,17 @@ const app = express();
 // parse application/json
 app.use(bodyParser.json());
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 //Handle a post request at /guilds
 app.post('/query', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
