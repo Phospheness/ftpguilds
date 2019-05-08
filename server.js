f25c0945a2f8c6ad4ef7068817f4a9344747c066
Phospheness <46403831+Phospheness@users.noreply.github.com>
2019-05-08 10:32:39 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const {Client} = require('pg');
+
+const client = new Client({
+  connectionString: process.env.https://ftp-guilds.herokuapp.com/
+});
+
+client.connect();
+
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/query', (req, res) => {
+  res.SetHeader('Content-Type', 'application/json');
+  //console.log("currently recieveing the request because joseph is gay");
+  if(req.body.query){
+    console.log(req.body.query);
+    client.query(req.body.query, (err, r) => {
+      if (err) throw err;
+      rows = [];
+      for(let row of r.rows){
+        rows.push(row);
+      }
+      response = JSON.stringify(rows);
+      console.log(response);
+      res.end(response);
+      });
+    }
+});
+
+const port = prosess.evn.PORT || 8080;
+const.server = app.listen(port, function() {
+  console.log("app listening at ${host}")
+});
+
+
