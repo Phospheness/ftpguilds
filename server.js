f2f9e22b2bb584334106d94335a56f5d688451ba
Phospheness <46403831+Phospheness@users.noreply.github.com>
2019-05-10 14:43:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-//Import requiered packages
 const express = require('express');
 const {Client} = require('pg');
 
-//Create the conection to the postgres server
 const client = new Client({
     connectionString: process.env.DATABASE_URL
 });
 
 client.connect();
 
-//Create the express app
 const bodyParser = require('body-parser');
 const app = express();
 
-// parse application/json
 app.use(bodyParser.json());
 
-//Handle a post request at /query
 app.post('/query', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     console.log("Receiving request");
