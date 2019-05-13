1126df127b5ad1690f6718efd80ac432d027a6a3
Phospheness <46403831+Phospheness@users.noreply.github.com>
2019-05-13 12:51:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/client/groups.php', function(req, res){
         res.send(rows); 
     });
 });
-app.get('/client/getgroupinfo.php?gid=:id', function(req, res){
+app.get('/client/getgroupinfo.php?gid/:id', function(req, res){
     var Lawa = req.params.id;
     connection.connect();
     connection.query('SELECT ' + Lawa + ' FROM guilds', function(err, rows, fields){
